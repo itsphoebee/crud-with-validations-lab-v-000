591c9f101063cb51a8e40f4e0d3fa44d186ee201
Phoebe Lau <phoebelau7@gmail.com>
2018-01-29 20:41:58 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,7 @@ class Song < ActiveRecord::Base
   validates_uniqueness_of :title, scope: [:release_year, :artist_name]
   validates :artist_name, presence: true
   validate :must_have_release_year_if_released, :release_year_cannot_be_in_the_future
-  
+
   def must_have_release_year_if_released
     if released? && release_year.blank?
       errors.add(:release_year, "cannot be blank if song was released" )
@@ -14,16 +14,6 @@ class Song < ActiveRecord::Base
   def release_year_cannot_be_in_the_future
     if release_year.present? && release_year > Time.now.year
       errors.add(:release_year, "cannot be in the future")
-    end
-  end
-
-  def cannot_release_same_song_twice_a_year
-    if title.present? && release_year.present? && artist_name.present?
-      a = Song.find_by(title: title, release_year: release_year, artist_name: artist_name)
-      if a
-      #  binding.pry
-        errors.add(:title, "cannot release same song twice in a year")
-      end
     end
   end
 
