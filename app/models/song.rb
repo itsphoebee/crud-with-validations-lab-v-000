6f68b62043d4a8f65d3cc8b6c4755a5cbd48f43b
Phoebe Lau <phoebelau7@gmail.com>
2018-01-29 18:27:51 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,12 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
   #validates :released, inclusion: {in: %w(true false)}
   validates :artist_name, presence: true
+  validate :must_have_release_year_if_released
+
+  def must_have_release_year_if_released
+    if released.valid? && release_year.blank?
+      errors.add(:release_year, "cannot be blank if song was released" )
+    end
+  end
+
 end
