eb282e24f0ae87330895e32ed3e68189b96a9faa
Phoebe Lau <phoebelau7@gmail.com>
2018-01-29 20:01:32 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -16,7 +16,7 @@ class Song < ActiveRecord::Base
   end
 
   def cannot_release_same_song_twice_a_year
-    if title.present? && release_year.present? && artist_name.present?
+    if !title.present? && !release_year.present? && !artist_name.present?
       a = Song.find_by(title: title, release_year: release_year, artist_name: artist_name)
       if a
         errors.add(:title, "cannot release same song twice in a year")
