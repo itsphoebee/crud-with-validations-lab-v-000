f387125ad59d6ed37b58bd0f45c2ccf079a70342
Phoebe Lau <phoebelau7@gmail.com>
2018-01-29 18:10:38 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,27 @@
 class SongsController < ApplicationController
+  
   def index
     @songs = Song.all
   end
+  
   def new
     @song = Song.new
   end
+
+  def create
+    @song = Song.create(song_params)
+    if @song.valid?
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def song_params
+    
+  end
+
+  
 end
