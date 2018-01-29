50d7ea1645f3ec30505480a67d013e765f4f535c
Phoebe Lau <phoebelau7@gmail.com>
2018-01-29 18:21:31 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :Released, inclusion: {in: %w(true false)}
+  validates :released, inclusion: {in: %w(true false)}
   validates :artist_name, presence: true
 end
