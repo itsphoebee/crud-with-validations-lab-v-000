6f7c9184d9e2ed6bcac239e25351dfa647ca6a66
Phoebe Lau <phoebelau7@gmail.com>
2018-01-29 18:41:58 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -30,6 +30,9 @@ class SongsController < ApplicationController
     @song.update(song_params)
     if @song.valid?
       redirect_to song_path(@song)
+    else
+      render :edit
+    end
   end
 
   def destroy
